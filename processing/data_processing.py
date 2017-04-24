01d728c1fd6a881a43fe5afd77067795f21d7471
JingheZ <jz4kg@virginia.edu>
2017-04-24 14:07:58 -0400

    process visit info

--- processing/data_processing.py
+++ processing/data_processing.py
@@ -44,7 +44,7 @@ def process_orders(filename, colnames, colnames_selected, item_colname):
 def get_orders_visit_info(data_med, data_proc):
     visit_info_med = data_med[['ptid', 'vid', 'anon_adm_date', 'anon_dis_date', 'VisitDXs', 'cdrIPorOP', 'timeline']]
     visit_info_proc = data_proc[['ptid', 'vid', 'anon_adm_date', 'anon_dis_date', 'VisitDXs', 'cdrIPorOP', 'timeline']]
-    visit_info_orders = pd.concat((visit_info_med, visit_info_proc), axis=1)
+    visit_info_orders = pd.concat((visit_info_med, visit_info_proc), axis=0)
     visit_info_orders = visit_info_orders.drop_duplicates()
     with open('./data/orders_visit_info.pickle', 'wb') as f:
         pickle.dump(visit_info_orders, f)
@@ -55,7 +55,7 @@ def get_orders_visit_info(data_med, data_proc):
 def get_orders_pt_info(data_med, data_proc):
     pt_info_med = data_med[['ptid', 'age', 'sex']]
     pt_info_proc = data_proc[['ptid', 'age', 'sex']]
-    pt_info_orders = pd.concat((pt_info_med, pt_info_proc), axis=1)
+    pt_info_orders = pd.concat((pt_info_med, pt_info_proc), axis=0)
     pt_info_orders = pt_info_orders.drop_duplicates()
     with open('./data/orders_pt_info.pickle', 'wb') as f:
         pickle.dump(pt_info_orders, f)
