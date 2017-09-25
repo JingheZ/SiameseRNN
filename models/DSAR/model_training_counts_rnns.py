04fc0f86b40d2c91afa44440ea14e7b8b3b56fe9
JingheZ <jz4kg@virginia.edu>
2017-09-24 20:53:49 -0400

    change class weight

--- models/DSAR/model_training_counts_rnns.py
+++ models/DSAR/model_training_counts_rnns.py
@@ -13,9 +13,236 @@ import torch.optim as optim
 # from models.Patient2Vec import Patient2Vec
 from torch.autograd import Variable
 from gensim.models import Word2Vec
-from models.DSAR.Baselines import RNNmodel, LRmodel, MLPmodel, RETAIN
+# from models.DSAR.Baselines import RNNmodel, RETAIN
 import numpy as np
 from sklearn import metrics
+
+
+class RNNmodel(nn.Module):
+    """
+    A recurrent NN
+    """
+
+    def __init__(self, input_size, embed_size, hidden_size, n_layers, initrange, output_size, rnn_type, seq_len, bi, ct, dropout_p=0.5):
+        """
+        Initilize a recurrent autoencoder
+        """
+        super(RNNmodel, self).__init__()
+
+        # Embedding
+        self.embed = nn.Linear(input_size, embed_size, bias=False)
+        # RNN
+        self.rnn = getattr(nn, rnn_type)(embed_size, hidden_size, n_layers, dropout=dropout_p,
+                                             batch_first=True, bias=True, bidirectional=bi)
+        self.b = 1
+        if bi:
+            self.b = 2
+        self.linear = nn.Linear(hidden_size * self.b, output_size, bias=True)
+        self.tanh = nn.Hardtanh()
+        self.init_weights(initrange)
+        self.input_size = input_size
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.seq_len = seq_len
+        self.ct = ct
+        self.func_softmax = nn.Softmax()
+        self.func_sigmoid = nn.Sigmoid()
+        self.func_tanh = nn.Hardtanh(0, 1)
+        # Add dropout
+        self.dropout_p = dropout_p
+        self.dropout = nn.Dropout(p=self.dropout_p)
+
+    def init_weights(self, initrange=1):
+        """
+        weight initialization
+        """
+        for param in self.parameters():
+            param.data.uniform_(-initrange, initrange)
+            # param.data.normal_(0, 1)
+
+    def embedding_layer(self, inputs, inputs_demoips):
+        if self.ct:
+            inputs_agg = inputs
+        else:
+            inputs_agg = torch.sum(inputs, dim=2)
+            inputs_agg = torch.squeeze(inputs_agg, dim=2)
+        embedding = []
+        for i in range(self.seq_len):
+            embedded = self.embed(torch.cat((inputs_agg[:, i], inputs_demoips), 1))
+            embedding.append(embedded)
+        embedding = torch.stack(embedding)
+        embedding = torch.transpose(embedding, 0, 1)
+            # embedding = self.tanh(embedding)
+        return embedding
+
+    def encode_rnn(self, embedding, batch_size):
+        self.weight = next(self.parameters()).data
+        init_state = (Variable(self.weight.new(self.n_layers * self.b, batch_size, self.hidden_size).zero_()))
+        # embedding_d = self.dropout(embedding)
+        outputs_rnn, states_rnn = self.rnn(embedding, init_state)
+        return outputs_rnn
+
+    def forward(self, inputs, inputs_demoips, batch_size):
+        """
+        the recurrent module
+        """
+        # Embedding
+        embedding = self.embedding_layer(inputs, inputs_demoips)
+        # embedding = torch.transpose(inputs, 1, 2)
+        # RNN
+        states_rnn = self.encode_rnn(embedding, batch_size)
+        # linear for context vector to get final output
+        linear_y = self.linear(states_rnn[:, -1])
+        out = self.func_softmax(linear_y)
+        # out = self.func_sigmoid(linear_y)
+        # out = self.func_tanh(linear_y)
+        return out, [states_rnn, embedding, linear_y]
+
+
+class RETAIN(nn.Module):
+    """
+    A recurrent autoencoder with an encoder, recurrent module, and a decoder.
+    In addition, a linear layer is on top of each decode step and the weights are shared at these step.
+    RETAIN in PyTorch (Code written by Jinghe Zhang)
+
+    RETAIN: An interpretable predictive model for healthcare using reverse time attention mechanism
+    https://arxiv.org/abs/1608.05745
+    """
+
+    def __init__(self, input_size, embed_size, hidden_size, n_layers, initrange, output_size, rnn_type, seq_len, dropout_p=0.5):
+        """
+        Initilize a recurrent autoencoder
+        """
+        super(RETAIN, self).__init__()
+
+        # Embedding
+        self.embed = nn.Linear(input_size, embed_size, bias=False)
+        # Forward RNN
+        self.rnn_l = getattr(nn, rnn_type)(embed_size, hidden_size, n_layers, dropout=dropout_p,
+                                             batch_first=True, bias=True)
+        # Backward RNN
+        self.rnn_r = getattr(nn, rnn_type)(embed_size, hidden_size, n_layers, dropout=dropout_p,
+                                             batch_first=True, bias=True)
+        # linear of attention
+        self.linear_att_func1 = nn.Linear(hidden_size, 1, bias=True)
+        self.linear_att_func2 = nn.Linear(hidden_size, embed_size, bias=True)
+        # final linear layer
+        self.linear = nn.Linear(embed_size, output_size, bias=True)
+
+        self.init_weights(initrange)
+        self.input_size = input_size
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.seq_len = seq_len
+        self.func_softmax = nn.Softmax()
+        self.func_sigmoid = nn.Sigmoid()
+        self.func_tanh = nn.Hardtanh()
+        # Add dropout
+        self.dropout_p = dropout_p
+        if self.dropout_p > 0:
+            self.dropout = nn.Dropout(p=self.dropout_p)
+
+    def init_weights(self, initrange):
+        """
+        weight initialization
+        """
+        for param in self.parameters():
+            param.data.uniform_(-initrange, initrange)
+
+    def embedding_layer(self, inputs, inputs_demoips):
+        # embedding_f = []
+        embedding_b = []
+        for i in range(self.seq_len):
+            # embedded = self.embed(inputs[:, :, i])
+            embedded = self.embed(torch.cat((inputs[:, i], inputs_demoips), 1))
+            # embedding_f.append(embedded)
+            embedding_b.insert(0, embedded)
+        embedding_b = torch.stack(embedding_b)
+        embedding_b = torch.transpose(embedding_b, 0, 1)
+        # embedding_b = self.func_tanh(embedding_b)
+        return embedding_b
+
+    def encode_rnn_l(self, embedding, batch_size):
+        self.weight = next(self.parameters()).data
+        init_state = (Variable(self.weight.new(self.n_layers, batch_size, self.hidden_size).zero_()))
+        # embedding = self.dropout(embedding)
+        outputs_rnn, states_rnn = self.rnn_l(embedding, init_state)
+        return outputs_rnn
+
+    def encode_rnn_r(self, embedding, batch_size):
+        self.weight = next(self.parameters()).data
+        init_state = (Variable(self.weight.new(self.n_layers, batch_size, self.hidden_size).zero_()))
+        # embedding = self.dropout(embedding)
+        outputs_rnn, states_rnn = self.rnn_r(embedding, init_state)
+        return outputs_rnn
+
+    def add_visit_attention(self, states_rnn):
+        # attention
+        linear_att = []
+        for i in range(self.seq_len):
+            att = self.linear_att_func1(states_rnn[:, i, :])
+            linear_att.append(att)
+        linear_att = torch.stack(linear_att)
+        linear_att = torch.squeeze(linear_att)
+        linear_att = torch.transpose(linear_att, 0, 1)
+        alpha = self.func_softmax(linear_att)
+        return alpha
+
+    def add_variable_attention(self, states_rnn):
+        # attention
+        linear_att = []
+        for i in range(self.seq_len):
+            att = self.linear_att_func2(states_rnn[:, i, :])
+            linear_att.append(att)
+        linear_att = torch.stack(linear_att)
+        linear_att = torch.squeeze(linear_att)
+        linear_att = torch.transpose(linear_att, 0, 1)
+        beta = self.func_tanh(linear_att)
+        return beta
+
+    def get_context_vector(self, alpha, beta, vs):
+        matrix = beta * vs # element-wise multiplication
+        matrix = torch.transpose(matrix, 1, 2)
+        alpha = torch.unsqueeze(alpha, 2)
+        context = torch.bmm(matrix, alpha)
+        context = torch.squeeze(context)
+        return context
+
+    def make_step_pred(self, alpha, beta, embedding):
+        # linear for context vector to get output at each step
+        out = []
+        for i in range(self.seq_len):
+            context = self.get_context_vector(alpha[:, :i+1], beta[:, :i+1, :], embedding[:, :i+1, :])
+            linear_y = self.linear(context)
+            out.append(self.func_sigmoid(linear_y))
+        out = torch.stack(out)
+        out = torch.squeeze(out)
+        out = torch.transpose(out, 0, 1)
+        return out
+
+    def forward(self, inputs, inputs_demoips, batch_size):
+        """
+        the recurrent module of the autoencoder
+        """
+        # Embedding
+        embedding_b = self.embedding_layer(inputs, inputs_demoips)
+        # RNN on the left for variable level attention, backward RNN
+        states_rnn_l = self.encode_rnn_l(embedding_b, batch_size)
+        # RNN on the right for visit level attention, backward RNN
+        states_rnn_r = self.encode_rnn_r(embedding_b, batch_size)
+        # Add attentions
+        alpha = self.add_visit_attention(states_rnn_r)
+        beta = self.add_variable_attention(states_rnn_l)
+        # linear for context vector to get final output
+        # context = self.get_context_vector(alpha, beta, embedding_b)
+        # linear_y = self.linear(context)
+        # out = self.func_softmax(linear_y)
+        out = self.make_step_pred(alpha, beta, embedding_b)
+        return out, [alpha, beta]
+        # return alpha, beta
 
 
 def create_batch(step, batch_size, data_x, data_demoip, data_y):
