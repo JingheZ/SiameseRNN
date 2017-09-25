a26780804808da7de2c5e360c93d99708f62ce18
JingheZ <jz4kg@virginia.edu>
2017-09-24 20:30:04 -0400

    change class weight

--- models/DSAR/model_training_counts_LRandMLP.py
+++ models/DSAR/model_training_counts_LRandMLP.py
@@ -188,10 +188,10 @@ if __name__ == '__main__':
     decay = 0.005
     interval = 10
     initrange = 1
-    mlp_hidden_size1 = 128
+    mlp_hidden_size1 = 256
     # mlp_hidden_size2 = 1024
 
-    batch_size = 100
+    batch_size = 200
     epoch_max = 5 # training for maximum 3 epochs of training data
     n_iter_max_dev = 1000 # if no improvement on dev set for maximum n_iter_max_dev, terminate training
     train_iters = len(train_ids)
