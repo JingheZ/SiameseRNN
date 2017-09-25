d7d2a8477b0b5ed33a8e353f1899c325c27a121c
JingheZ <jz4kg@virginia.edu>
2017-09-24 20:40:54 -0400

    change class weight

--- models/DSAR/model_training_counts_LRandMLP.py
+++ models/DSAR/model_training_counts_LRandMLP.py
@@ -205,7 +205,7 @@ if __name__ == '__main__':
     if model_type == 'MLP':
         model = MLPmodel(input_size, mlp_hidden_size1, output_size, initrange)
 
-    criterion = nn.CrossEntropyLoss(weight=torch.FloatTensor([1, 25]))
+    criterion = nn.CrossEntropyLoss(weight=torch.FloatTensor([1, 30]))
     optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=decay)
     model_path = './saved_models/model_' + model_type + '.dat'
     print('Start Training...')
