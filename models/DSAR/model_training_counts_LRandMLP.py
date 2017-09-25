4f86a3366d6141d2170bc32518b73ce0eb209949
JingheZ <jz4kg@virginia.edu>
2017-09-24 20:24:53 -0400

    change class weight

--- models/DSAR/model_training_counts_LRandMLP.py
+++ models/DSAR/model_training_counts_LRandMLP.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
     output_size = 2
     drop = 0.0
     learning_rate = 0.0005
-    decay = 0.02
+    decay = 0.005
     interval = 10
     initrange = 1
     mlp_hidden_size1 = 256
