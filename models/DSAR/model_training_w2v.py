8a3710206486419d77b91ccf518669466fcb3fce
JingheZ <jz4kg@virginia.edu>
2017-09-25 19:43:00 -0400

    train the RNN models with counts and word embeddings

--- models/DSAR/model_training_w2v.py
+++ models/DSAR/model_training_w2v.py
@@ -368,21 +368,21 @@ if __name__ == '__main__':
     # Model hyperparameters
     # model_type = 'rnn-rt'
     input_size = size + 3
-    embedding_size = 300
+    embedding_size = 100
     hidden_size = 128
     n_layers = 1
     seq_len = 12
     output_size = 2
     rnn_type = 'GRU'
     drop = 0.0
-    learning_rate = 0.001
+    learning_rate = 0.0005
     decay = 0.005
     interval = 100
     initrange = 1
     att_dim = 1
     n_hops = 5
     batch_size = 100
-    epoch_max = 3 # training for maximum 3 epochs of training data
+    epoch_max = 5 # training for maximum 3 epochs of training data
     n_iter_max_dev = 1000 # if no improvement on dev set for maximum n_iter_max_dev, terminate training
     train_iters = len(train_ids)
 
