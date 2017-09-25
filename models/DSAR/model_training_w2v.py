aa883afac61cd914f847b7efbb0e95dc28b1804d
JingheZ <jz4kg@virginia.edu>
2017-09-25 01:36:39 -0400

    train RNN model - w2v

--- models/DSAR/model_training_w2v.py
+++ models/DSAR/model_training_w2v.py
@@ -369,7 +369,7 @@ if __name__ == '__main__':
     # Model hyperparameters
     # model_type = 'rnn-rt'
     input_size = size + 3
-    embedding_size = 150
+    embedding_size = 100
     hidden_size = 128
     n_layers = 1
     seq_len = 12
