41637315c46a133cbab739d6865f50cc520419d9
JingheZ <jz4kg@virginia.edu>
2017-09-25 21:47:36 -0400

    train RNN model - w2v

--- models/DSAR/model_training_w2v.py
+++ models/DSAR/model_training_w2v.py
@@ -382,7 +382,7 @@ if __name__ == '__main__':
     att_dim = 1
     n_hops = 5
     batch_size = 100
-    epoch_max = 1 # training for maximum 3 epochs of training data
+    epoch_max = 8 # training for maximum 3 epochs of training data
     n_iter_max_dev = 1000 # if no improvement on dev set for maximum n_iter_max_dev, terminate training
     train_iters = len(train_ids)
 
