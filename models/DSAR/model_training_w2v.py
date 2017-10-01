508c928d5d498903009245af87d26d456ec94722
JingheZ <jz4kg@virginia.edu>
2017-10-01 19:47:46 -0400

    change to every 3month

--- models/DSAR/model_training_w2v.py
+++ models/DSAR/model_training_w2v.py
@@ -366,7 +366,7 @@ if __name__ == '__main__':
     # model_type = 'rnn-rt'
     input_size = size + 3
     embedding_size = 300
-    hidden_size = 128
+    hidden_size = 256
     n_layers = 1
     seq_len = int(12 / l)
     output_size = 2
