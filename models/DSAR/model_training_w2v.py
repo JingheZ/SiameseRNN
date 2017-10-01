9b622644b9c00a6038bca0845e0c787a6bbf6952
JingheZ <jz4kg@virginia.edu>
2017-09-30 22:58:44 -0400

    train RNN model - w2v

--- models/DSAR/model_training_w2v.py
+++ models/DSAR/model_training_w2v.py
@@ -369,20 +369,20 @@ if __name__ == '__main__':
     # model_type = 'rnn-rt'
     input_size = size + 3
     embedding_size = 300
-    hidden_size = 256
+    hidden_size = 128
     n_layers = 1
     seq_len = 12
     output_size = 2
     rnn_type = 'GRU'
     drop = 0.0
     learning_rate = 0.001
-    decay = 0.005
+    decay = 0.01
     interval = 100
     initrange = 1
     att_dim = 1
     n_hops = 5
     batch_size = 100
-    epoch_max = 5 # training for maximum 3 epochs of training data
+    epoch_max = 10 # training for maximum 3 epochs of training data
     n_iter_max_dev = 1000 # if no improvement on dev set for maximum n_iter_max_dev, terminate training
     train_iters = len(train_ids)
 
