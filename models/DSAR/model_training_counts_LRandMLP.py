7eaf21de393583939930f4d8f878dbbe42fe9182
JingheZ <jz4kg@virginia.edu>
2017-10-01 22:57:01 -0400

    change to every 3month

--- models/DSAR/model_training_counts_LRandMLP.py
+++ models/DSAR/model_training_counts_LRandMLP.py
@@ -185,14 +185,14 @@ if __name__ == '__main__':
     output_size = 2
     drop = 0.0
     learning_rate = 0.001
-    decay = 0.005
-    interval = 10
+    decay = 0.01
+    interval = 100
     initrange = 1
     mlp_hidden_size1 = 256
     # mlp_hidden_size2 = 1024
 
     batch_size = 100
-    epoch_max = 2 # training for maximum 3 epochs of training data
+    epoch_max = 1 # training for maximum 3 epochs of training data
     n_iter_max_dev = 1000 # if no improvement on dev set for maximum n_iter_max_dev, terminate training
     train_iters = len(train_ids)
 
