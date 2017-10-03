458d1dd537edf631664b5a61c7de73823f804099
JingheZ <jz4kg@virginia.edu>
2017-10-03 15:10:13 -0400

    train the RNN models with counts and word embeddings

--- models/DSAR/model_training_w2v.py
+++ models/DSAR/model_training_w2v.py
@@ -365,7 +365,7 @@ if __name__ == '__main__':
     # Model hyperparameters
     # model_type = 'rnn-rt'
     input_size = size + 3
-    embedding_size = 300
+    embedding_size = 500
     hidden_size = 256
     n_layers = 1
     seq_len = int(12 / l)
