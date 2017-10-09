1ce2b35b44387a34bec9cd9889cdac64d242a20e
JingheZ <jz4kg@virginia.edu>
2017-10-09 03:26:52 -0400

    change to every 3month

--- models/DSAR/model_training_counts_LRandMLP.py
+++ models/DSAR/model_training_counts_LRandMLP.py
@@ -183,8 +183,8 @@ if __name__ == '__main__':
     input_size = len(features) + 3
     output_size = 2
     drop = 0.0
-    learning_rate = 0.0005
-    decay = 0.01
+    learning_rate = 0.001
+    decay = 0.05
     interval = 100
     initrange = 1
     mlp_hidden_size1 = 256
