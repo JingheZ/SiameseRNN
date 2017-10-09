6754332967abe56be7cfd039cb9c97a7b8bbe9f0
JingheZ <jz4kg@virginia.edu>
2017-10-09 03:26:27 -0400

    change to every 3month

--- models/DSAR/model_training_w2v.py
+++ models/DSAR/model_training_w2v.py
@@ -494,14 +494,14 @@ if __name__ == '__main__':
     # Model hyperparameters
     # model_type = 'rnn-rt'
     input_size = size + 3
-    embedding_size = 300
+    embedding_size = 200
     hidden_size = 256
     n_layers = 1
     seq_len = int(12 / l)
     output_size = 2
     rnn_type = 'GRU'
     drop = 0.0
-    learning_rate = 0.0005
+    learning_rate = 0.001
     decay = 0.01
     interval = 50
     initrange = 1
