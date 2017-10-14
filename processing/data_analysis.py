f04861eca979f5c322287f4034a844cc0b5c86ea
JingheZ <jz4kg@virginia.edu>
2017-10-14 18:26:14 -0400

    interpret results

--- processing/data_analysis.py
+++ processing/data_analysis.py
@@ -3,7 +3,7 @@ import pandas as pd
 import pickle
 import seaborn as sns
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 def load_data(filename, colnames):
     data = pd.read_csv(filename, delimiter='\t', header=None, encoding="ISO-8859-1")
@@ -46,11 +46,11 @@ ages_pts.name = 'Age'
 ax = sns.distplot(ages_pts)
 
 #
-# with open('./results/example_pts_info.pickle', 'rb') as f:
-#     exmple, exmple_seq, seq_wts_exmple, code_wts_exmple = pickle.load(f)
-
-with open('example_pts_info.pickle', 'rb') as f:
+with open('./results/example_pts_info.pickle', 'rb') as f:
     exmple, exmple_seq, seq_wts_exmple, code_wts_exmple = pickle.load(f)
+
+# with open('example_pts_info.pickle', 'rb') as f:
+#     exmple, exmple_seq, seq_wts_exmple, code_wts_exmple = pickle.load(f)
 
 def prepare_for_heatmap(data):
     grp = []
@@ -75,32 +75,76 @@ dt1 = prepare_for_heatmap(code_wts_exmple[0])
 dt1.columns = ['name', 't1', 't2', 't4']
 dt1['t3'] = 0.0
 dt1 = dt1[['name', 't1', 't2', 't3', 't4']]
-dt1 = dt1.sort(['t4', 't3', 't2', 't1'], ascending=[0, 0, 0, 0])
+dt1 = dt1.sort(['name', 't4', 't3', 't2', 't1'], ascending=[1, 0, 0, 0, 0])
 dt1.index = dt1['name']
 del dt1['name']
-# dt1.loc['Weight'] = seq_wts_exmple[0]
-hm1 = sns.heatmap(dt1, vmin=0, vmax=0.2)
-hm1.savefig("example_heatmap1.png")
+dt1.loc['Weight'] = seq_wts_exmple[0]
+dt1 = dt1.ix[~dt1['t4'].between(0.01, 0.017)]
+dt1.index = list(dt1.index[:10]) + ['Coronary atherosclerosis & heart disease', 'Conduction disorders',
+                                    'Cardiac dysrhythmias', 'Diverticulosis and diverticulitis',
+                                    'Gastrointestinal hemorrhage', 'Acute and unspecified renal failure',
+                                    'Chronic kidney disease', 'Hyperplasia of prostate', 'Osteoarthritis',
+                                    'Other connective tissue disease', 'Other aftercare', 'Nutritional deficiencies',
+                                    'Disorders of lipid metabolism', 'Nutritional, endocrine, metabolic disorders',
+                                    'Deficiency and other anemia',
+                                    'Screening of mental health & substance abuse',
+                                    'Dizziness or vertigo',
+                                    'Hypertension with complications'] + \
+            ['Suture of skin and subcutaneous tissue', 'Other organ transplantation', 'Arterial blood gases',
+             'Microscopic examination', 'Diagnostic radiology and related techniques',
+             'Laboratory - Chemistry and hematology', 'Pathology', 'Other laboratory',
+             'OR therapeutic procedures; nose, mouth, pharynx', 'Other OR heart procedures',
+             'Embolectomy and endarterectomy of lower limbs', 'Therapeutic procedures; hemic & lymphatic system',
+             'OR therapeutic nervous system procedures', 'sequence-level weight']
+
+plt.figure(figsize=(32, 15))
+sns.set(font_scale=1.2)
+hm1 = sns.heatmap(dt1, vmin=0, vmax=0.35)
+plt.yticks(rotation=0)
+plt.xlabel('Time')
+plt.ylabel('')
+plt.savefig("./results/example_heatmap1.png")
 
 # example pt2
 dt2 = prepare_for_heatmap(code_wts_exmple[1])
 dt2.columns = ['name', 't1', 't2', 't3', 't4']
-dt2 = dt2.sort(['t4', 't3', 't2', 't1'], ascending=[0, 0, 0, 0])
+dt2 = dt2.sort(['name', 't4', 't3', 't2', 't1'], ascending=[1, 0, 0, 0, 0])
 dt2.index = dt2['name']
 del dt2['name']
-hm2 = sns.heatmap(dt2[['t1', 't2', 't3', 't4']].values, vmin=0, vmax=0.4)
-hm2.savefig("example_heatmap2.png")
+dt2.loc['Weight'] = seq_wts_exmple[1]
+dt2.index = ['Diagnostic Products', 'Coronary atherosclerosis & heart disease',
+             'Genitourinary symptoms', 'Spondylosis', 'Other connective tissue disease',
+             'Bone disease & musculoskeletal deformities', 'Malaise and fatigue',
+             'Diabetes mellitus without complication', 'Diabetes mellitus with complications',
+             'Disorders of lipid metabolism', 'Essential hypertension',
+             'Diagnostic procedures', 'Other organ transplantation',
+             'Therapeutic procedures on conjunctiva; cornea', 'Arterial blood gases',
+             'Microscopic examination', 'Other radioisotope scan',
+             'Other laboratory', 'Other OR heart procedures',
+             'Non-OR therapeutic nervous system procedures', 'Sequence-level weight']
+plt.figure(figsize=(30, 15))
+sns.set(font_scale=1.2)
+hm2 = sns.heatmap(dt2[['t1', 't2', 't3', 't4']], vmin=0, vmax=0.35)
+plt.yticks(rotation=0)
+plt.xlabel('Time')
+plt.ylabel('')
+plt.savefig("./results/example_heatmap2.png")
 
 # example pt3
 dt3 = prepare_for_heatmap(code_wts_exmple[2])
 dt3.columns = ['name', 't1', 't3', 't4']
 dt3['t2'] = 0.0
 dt3 = dt3[['name', 't1', 't2', 't3', 't4']]
-dt3 = dt3.sort(['t4', 't3', 't2', 't1'], ascending=[0, 0, 0, 0])
+dt3 = dt3.sort(['name', 't4', 't3', 't2', 't1'], ascending=[1, 0, 0, 0, 0])
 dt3.index = dt3['name']
 del dt3['name']
-hm3 = sns.heatmap(dt3[['t1', 't2', 't3', 't4']].values, vmin=0, vmax=0.3)
-hm3.savefig("example_heatmap3.png")
+dt3.loc['Weight'] = seq_wts_exmple[2]
+plt.figure(figsize=(15, 16))
+hm3 = sns.heatmap(dt3[['t1', 't2', 't3', 't4']], vmin=0, vmax=0.35)
+plt.yticks(rotation=0)
+plt.xlabel('Time')
+plt.ylabel('')
+hm3.savefig("./results/example_heatmap3.png")
 
 
 # ====== number of visits in the observation window ========
